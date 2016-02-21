40e4541a6e59924c4349bce1d0d05211498d3844
AllenIsCoding <pats4real@gmail.com>
2016-02-21 11:24:18 +0300

    Common LogViewer

---
+++ AistTrader/Common/Common/Controls/NlogViewerTarget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog.Common;
+using NLog.Targets;
+
+namespace Common.Controls
+{
+    [Target("NlogViewer")]
+    public class NlogViewerTarget : Target
+    {
+        public event Action<AsyncLogEventInfo> LogReceived;
+
+        protected override void Write(NLog.Common.AsyncLogEventInfo logEvent)
+        {
+            base.Write(logEvent);
+
+            if (LogReceived != null)
+                LogReceived(logEvent);
+        }
+    }
+}
