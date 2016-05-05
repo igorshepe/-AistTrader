b124747c94d5a28d2689632a3073c2fa23a75dd5
AllenIsCoding <pats4real@gmail.com>
2016-05-05 13:28:12 +0300

    mode to start strategy

--- Strategies/Strategies/Settings/CandleStrategySettings.cs
+++ Strategies/Strategies/Settings/CandleStrategySettings.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Strategies.Common;
-using Strategies.Strategies;
 
 namespace Strategies.Settings
 {
