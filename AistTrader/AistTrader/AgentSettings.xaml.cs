d1becd67f8c8a77d0ab50639c48daa51dc33fc72
AllenIsCoding <pats4real@gmail.com>
2016-05-12 11:17:01 +0300

    before working one

--- AistTrader/AistTrader/AgentSettings.xaml.cs
+++ AistTrader/AistTrader/AgentSettings.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Data;
@@ -13,10 +14,8 @@ using Strategies.Settings;
 
 namespace AistTrader
 {
-
-
-
-    public class AgentSettingParametrProperty
+    [Serializable]
+    public class AgentSettingParameterProperty
     {
         public  string Name { get; set; }
         public  decimal Parametr { get; set; }
@@ -29,6 +28,7 @@ namespace AistTrader
         public StrategyDefaultSettings Settings { get; set; }
         public CollectionView AgentSettingsCollectionView { get; set; }
         public ObservableCollection<object> AgentSettingsStorage { get; private set; }
+        
 
         // ReSharper restore MemberCanBePrivate.Global
 
@@ -38,29 +38,39 @@ namespace AistTrader
 
             SettingsStorage = settingsStorage;
             Settings = settings;
+            var type  = settings.GetType();
+            
             InitializeComponent();
             Settings.Load(SettingsStorage);
-            Type type = typeof (CandleStrategyDefaultSettings);
-            PropertyInfo[] properties = type.GetProperties();
+            //Type type = typeof (CandleStrategySettings);
+            PropertyInfo[] properties = type.GetProperties();   
             foreach (PropertyInfo property in properties)
             {
-                //var agentSettingsProperty = new AgentSettingParametrProperty();
-                //agentSettingsProperty.Name = property.Name;
-                //if (property.Name == "TimeFrame")
-                //    TimeSpan test =(TimeSpan)property.GetValue(Settings);
-                //    double sec = test.TotalSeconds;
-                //    agentSettingsProperty.Parametr = (decimal)sec;
-                //{
-                //    //agentSettingsProperty.Parametr = test;
-                //    //                    agentSettingsProperty.Parametr = (decimal)property.GetValue(Settings);
-                //}
-                //else
-                //    agentSettingsProperty.Parametr = (decimal)property.GetValue(Settings);
+                var agentSettingsProperty = new AgentSettingParameterProperty();
+                agentSettingsProperty.Name = property.Name;
                 
-                //agentSettingsProperty.UseInAgentName = false;
-                //AgentSettingsStorage.Add(agentSettingsProperty);
+                if (property.Name == "TimeFrame")
+                {
+                    TimeSpan ts = (TimeSpan)property.GetValue(Settings);
+                    agentSettingsProperty.Parametr = (decimal)ts.TotalSeconds;
+
+                   // if (SettingsStorage != null) SettingsStorage.Add(property.Name, ts);
+                }
+                
+                //{
+                    //agentSettingsProperty.Parametr = test;
+                    //                    agentSettingsProperty.Parametr = (decimal)property.GetValue(Settings);
+                //}
+                else
+                    agentSettingsProperty.Parametr = (decimal)property.GetValue(Settings);
+
+                agentSettingsProperty.UseInAgentName = false;
+                AgentSettingsStorage.Add(agentSettingsProperty);
             }
             AgentSettingsDG.ItemsSource = AgentSettingsStorage;
+            
+            
+
         }
 
         private void OkButtonClick(object sender, RoutedEventArgs e)
