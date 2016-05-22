79ff27cd3426dcff396c1fd5a6524b10d3967c36
AllenIsCoding <pats4real@gmail.com>
2016-05-23 02:25:30 +0300

    logger refactor

--- AistTrader/NlogViewer/NlogViewerTarget.cs
+++ AistTrader/NlogViewer/NlogViewerTarget.cs
@@ -1,10 +1,6 @@
 ﻿using NLog.Common;
 using NLog.Targets;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NlogViewer
 {
