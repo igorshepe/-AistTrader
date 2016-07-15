96c1d8796ba8e4ed0d5b132e2a218192c96468c0
AllenIsCoding <pats4real@gmail.com>
2016-07-15 15:34:35 +0300

    agent params ns

--- AistTrader/Common/Common/Params/AgentParams.cs
+++ AistTrader/Common/Common/Params/AgentParams.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Strategies.Common;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using StockSharp.BusinessEntities;
 
 namespace Common.Params
 {
-    [Serializable]
+    [DataContract(Namespace = "")]
     public class AgentParams
     {
         public AgentParams() { }
@@ -21,29 +22,43 @@ namespace Common.Params
             ToolTipName = toolTipName;
             PhantomParams = phantomParams;
         }
-
+        [DataMember()]
         public string Security { get; set; }
+        [DataMember()]
         public string AgentName { get; set; }
+        [DataMember()]
         public string ToolTipName { get; set; }
+        [DataMember()]
         public string AgentCompiledName { get; set; }
+        [DataMember()]
         public int ConnectionCount { get; set; }
+        [DataMember()]
         public string FriendlyName { get; set; }
+        [DataMember()]
         public SerializableDictionary<string, object> SettingsStorage { get; set; }
+        [DataMember()]
         public bool IsChecked { get; set; }
+        [DataMember()]
         public int Contracts { get; set; }
+        [DataMember()]
         public string GroupName { get; set; }
+        [DataMember()]
         //todo : СПРОСИТЬ У SS ЧЕ ЗА ФИНТ С КОНСТРУТОРОМ
         public string Amount { get; set; }
+        [DataMember()]
         public AgentPhantomParams PhantomParams { get; set; }
+        [DataMember()]
         public List<long> TransactionId {get;set;}
-        //public Security Tool { get; set; }
+        [DataMember()]
+        public Security Tool { get; set; }
     }
     public enum AgentWorkMode
     {
         Single,
         Group
     }
-    [Serializable]
+    
+    [DataContract(Namespace = "")]
     public class AgentPhantomParams
     {
         public AgentPhantomParams() { }
@@ -53,8 +68,11 @@ namespace Common.Params
             GroupName = groupName;
             Amount = amount;
         }
+        [DataMember()]
         public string AgentName { get; set; }
+        [DataMember()]
         public string GroupName { get; set; }
+        [DataMember()]
         public string Amount { get; set; }
     }
 
